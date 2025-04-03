b9dce03b55cbe653109c9e7216ea8dd868aae756
Collin Berg <berg.collin@gmail.com>
2025-04-02 22:29:14 -0400

    Refactored Header to account for the landing block

--- partials/header.php
+++ partials/header.php
@@ -1,17 +1,30 @@
-<section class="project__header">
-    <figure><?php //echo get_image_from_id( array( 'id' => get_post_thumbnail_id(), 'is_element' => true ) ); ?>
+<?php
+
+if(is_archive()):
+    $title = get_the_archive_title();
+elseif( is_home() ):
+    $title = get_the_title(get_option('page_for_posts', true));
+else:
+    $title = get_the_title();
+endif;
+?>
+<section class="page__header project__header">
+    <figure>
         <?php the_post_thumbnail('full'); ?>
     </figure>
     <div class="project__header--content">
         <div class="container--site">
             <div class="">
+                
                 <?php $market = get_field('market_category');		
                 if ( $market && ! is_wp_error( $market ) ) : ?>
                     <span class="project__capability"><?php echo $market[0]->post_title; ?></span>
                 <?php endif; ?>
+
+
                 <div class="project__header--info">
-                    <h1 class="headline"><?php the_title(); ?></h1>
-                    <?php if(get_post_type() == 'project'): ?>
+                    <h1 class="headline"><?php echo $title; ?></h1>
+                    <?php if(get_post_type() == 'project' && is_singular() ): ?>
                         <div class="project-details">
                             <span class="location"><?php the_field('project_location'); ?></span>
                             <?php $departments = get_field('performed_by'); 
@@ -28,7 +41,10 @@
                         
                     <?php endif; ?>
                 </div>
-                <?php generateBreadcrumb(); ?>
+                <?php 
+                if(is_singular() ):
+                    generateBreadcrumb();
+                endif; ?>
             </div>
         </div>  
     </div>
