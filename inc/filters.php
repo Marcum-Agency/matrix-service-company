b205491e93897f153660942f8c0003df46a833c2
Collin Berg <berg.collin@gmail.com>
2025-04-03 23:19:42 -0400

    Custom Archive Titles

--- inc/filters.php
+++ inc/filters.php
@@ -121,3 +121,18 @@ function wp_nav_menu_container_allowedtags_msc( $array ){
     $array[] = "nav";
     return $array;
 };
+
+function get_the_archive_title_msc ($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif (is_tax()) { //for custom post types
+        $title = sprintf(__('%1$s'), single_term_title('', false));
+    } elseif (is_post_type_archive()) {
+        $title = post_type_archive_title('', false);
+    }
+    return $title;
+};
