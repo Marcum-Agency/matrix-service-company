e441a59a4352349a02e3954424a405a5043ef772
Collin Berg <berg.collin@gmail.com>
2025-04-10 01:18:50 -0400

    removed giphy

--- 404.php
+++ 404.php
@@ -1,7 +1,8 @@
 <?php
-
 get_header();
+?>
+<div id="view-container" class="container container--view">
+    <p>Whoops! There's nothing here. But&hellip; There's no place like <a href="<?php echo get_home_url(); ?>" style="border-bottom:.1em solid var(--color--blue)">home</a>.</p>
+</div>
 
-?><div id="view-container" class="container container--view"><div class="giphy"><iframe src="https://giphy.com/embed/ycdNP5BQRz51S" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>Whoops! There's nothing here. But&hellip; There's no place like <a href="<?php echo get_home_url(); ?>" style="border-bottom:.1em solid var(--color--blue)">home</a>.</p></div><?php
-
-get_footer();
+<?php get_footer();
