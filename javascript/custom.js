cc4be21aa29dc7278163d9bf8f263f1b6615d7d1
Collin Berg <berg.collin@gmail.com>
2025-04-14 13:49:56 -0400

    Louis Changes

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,8 +1,11 @@
 jQuery(document).on("click", ".header--site .nav-menu-item .arrow" , function() {
-	jQuery(this).parent().toggleClass('active');
+	jQuery(this).parent().toggleClass('activate');
 	jQuery(this).next('.sub-menu').toggleClass('active');
 	jQuery('html').toggleClass('locked');
 });
+
+
+
 
 /*jQuery('.accordion h3').click(function(e) {
     jQuery(this).parent('.accordion_group').siblings().find('.accordion-panel').hide();
@@ -31,26 +34,68 @@ jQuery(document).ready(function () {
 
 /* Total Capabilities animation */
 
-  var controller = new ScrollMagic.Controller();
-  var tween1 = TweenMax.staggerFromTo(".split-list .scroll-list li", 1, {opacity: 50}, {opacity: 100}, 1);
-  
-  var scene = new ScrollMagic.Scene({triggerElement: ".split-list", duration: 200+"%",triggerHook: 0})
-  .setPin(".split-list")
-   //.addIndicators({name: "1 (duration: 100)"})
-  .addTo(controller);
-  
-  
-  var scene = new ScrollMagic.Scene({triggerElement: ".split-list", duration:200+"%",offset: 400})
-  .setTween(tween1)
-  .addTo(controller);
+const controller = new ScrollMagic.Controller();
+const list = document.querySelector('.scroll-list');
+const items = document.querySelectorAll('.scroll-list li');
 
-/*animation partb*/
-  var controllerb = new ScrollMagic.Controller();
-  var tweenb = TweenMax.staggerFromTo(".split-list .scroll-list li", 1, {top: 50+'%'}, {top: 0+'%'}, 1);
-  
-  var scene = new ScrollMagic.Scene({triggerElement: ".split-list", duration:170+"%",offset: 400})
-  .setTween(tweenb)
+const totalSlides = items.length;
+const liHeight = 90;
+const scrollPerItem = window.innerHeight;
+const initialOffset = 90;
+
+
+const tl = new TimelineMax();
+tl.set(list, { y: initialOffset });
+
+items.forEach((_, index) => {
+  const targetY = initialOffset - (liHeight * (index + 1));
+  tl.to(list, 1, { y: targetY });
+});
+
+
+new ScrollMagic.Scene({
+  triggerElement: ".split-list",
+  triggerHook: 0,
+  duration: scrollPerItem * totalSlides
+})
+.setPin(".split-list")
+.setTween(tl)
+.addTo(controller);
+
+
+items.forEach((li, index) => {
+  new ScrollMagic.Scene({
+    triggerElement: ".split-list",
+    triggerHook: 0.5,
+    offset: (index + 1) * scrollPerItem,
+    duration: scrollPerItem
+  })
+  .on("enter", () => {
+    items.forEach(el => el.classList.remove("active"));
+    li.classList.add("active");
+  })
   .addTo(controller);
+});
+
+
+new ScrollMagic.Scene({
+  triggerElement: ".split-list",
+  triggerHook: 0.5,
+  offset: 0,
+  duration: scrollPerItem
+})
+.on("leave", (event) => {
+  if (event.scrollDirection === "REVERSE") {
+    items.forEach(el => el.classList.remove("active"));
+  }
+})
+.addTo(controller);
+
+
+
+
+
+
   
 
 /* image slot  animation */
@@ -184,7 +229,14 @@ jQuery(document).ready(function(){
   jQuery(window).scroll(function(){
     jQuery(".reveal").each(function(){
       var x = jQuery(window).scrollTop() + jQuery(window).height();
-      var y = jQuery(this).offset().top + (jQuery(this).height() / 2);
+
+
+			if (jQuery(window).width() < 768) {
+			  var y = jQuery(this).offset().top + (jQuery(this).height() / 4);
+			}
+			else {
+			  var y = jQuery(this).offset().top + (jQuery(this).height() / 2);
+			}
 
       if (x >= y) {
         jQuery(this).addClass('reveal_visible');
@@ -326,5 +378,37 @@ var scroller = new SmoothScroll({
 	}, 500); 
     
 });*/
+
+
+
+jQuery('.testimonial blockquote p strong').each(function() {
+  var text = jQuery(this).text();
+  var wrappedText = '';
+  for (var i = 0; i < text.length; i++) {
+    wrappedText 	= '<span>' + text + '</span>';
+  }
+  jQuery(this).html(wrappedText);
+});
+
+
+
+var x = 1;
+
+function updateClass() {
+  let a = jQuery(".view--capabilities #hero-slider h2 strong.active");
+  a.removeClass("active");
+  a = a.next(".view--capabilities #hero-slider h2 strong");
+  if (a.length == 0)
+    a = jQuery(".view--capabilities #hero-slider h2 strong").first();
+  a.addClass("active");
+}
+
+jQuery(document).ready(function() {
+  setInterval(function() {
+    updateClass();
+  }, x * 4000);
+});
+
+
 
 
